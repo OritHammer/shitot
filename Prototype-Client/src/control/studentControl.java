0d15aad7716caddb4d00d500022e92bb54279ce3
lior hammer <lior hammer@DESKTOP-812DTM7>
2018-05-27 14:29:38 +0300

    adding variables  declaration to studentControl

--- Prototype-Client/src/control/studentControl.java
+++ Prototype-Client/src/control/studentControl.java
@@ -1,5 +1,45 @@
 package control;
 
-public class studentControl {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ComboBoxBase;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
+public class studentControl  extends UserControl implements Initializable{
+
+	@FXML
+	private Label userNameLabel ;
+	@FXML 
+	private Label authorLabel ;
+	@FXML
+	private Label dateLabel ;
+	@FXML
+	private Label newUpdateLable ;
+	
+		
+	@FXML 
+	private ComboBox<String> choosingSubject;
+	@FXML 
+	private ComboBox<String> orderChoosingSubject; 
+	@FXML 
+	private ComboBox<String> chooseExam; 
+	
+	@FXML 
+	private TextField manualExamCodeField; 
+	@FXML 
+	private TextField examCodeField; 
+	
+	
+	@FXML
+	private TableView<String> examsTable;
+	
+	
+	
+	
+	
+	
 }
