d7928e08248c91b6f1490244071b83c7e5338b95
tomza <tomza@DESKTOP-7LUTRR6>
2018-05-27 23:43:27 +0300

    tom

--- Prototype-Client/src/control/UserControl.java
+++ Prototype-Client/src/control/UserControl.java
@@ -26,186 +26,188 @@ import javafx.stage.Stage;
 
 public class UserControl implements Initializable
 {
- @FXML
- private TextField userName;
- @FXML
- private PasswordField password;
- @FXML
- private Label userNameError;
- @FXML
- private Label passwordError;
- @FXML
- private Label loginError;
- @FXML
- private Label noUser;
- @FXML
- private Button LoginBtn;
- 
- @FXML
- private ImageView LoginButton;
- 
- @FXML private javafx.scene.control.Button closeButton;
+	@FXML
+	private TextField userName;
+	@FXML
+	private PasswordField password;
+	@FXML
+	private Label userNameError;
+	@FXML
+	private Label passwordError;
+	@FXML
+	private Label loginError;
+	@FXML
+	private Label noUser;
+	@FXML
+	private Button LoginBtn;
+	
+	@FXML
+	private ImageView LoginButton;
+	
+	@FXML private javafx.scene.control.Button closeButton;
 
- 
- 
- Parent home_page_parent;
-  Scene home_page_scene;
-  Stage app_stage;
- 
-  static Thread th;
- public void closeButtonAction(ActionEvent e) throws IOException{
-     // get a handle to the stage
-     //Stage stage = (Stage) closeButton.getScene().getWindow();
-    ((Node)e.getSource()).getScene().getWindow().hide(); //hiding primary window
-    Stage primaryStage = new Stage();
-    FXMLLoader loader = new FXMLLoader();
-    Pane root = loader.load(getClass().getResource("/boundary/HomeScreenTeacher.fxml").openStream());
-    Scene scene = new Scene(root);   
-    primaryStage.setScene(scene);  
-    primaryStage.show();
-     // do what you have to do
-     //stage.close();
- }
- 
- protected ChatClient chat;
- 
- String[] messageToServer = new String[3];
- / connections variables /
- protected String ip ;// server ip
- 
- final public static int DEFAULT_PORT = 5555;
+	
+	
+	Parent home_page_parent;
+	 Scene home_page_scene;
+	 Stage app_stage;
+	
+	 static Thread th;
+	public void closeButtonAction(ActionEvent e) throws IOException{
+	    // get a handle to the stage
+	    //Stage stage = (Stage) closeButton.getScene().getWindow();
+		  ((Node)e.getSource()).getScene().getWindow().hide(); //hiding primary window
+		  Stage primaryStage = new Stage();
+		  FXMLLoader loader = new FXMLLoader();
+		  Pane root = loader.load(getClass().getResource("/boundary/HomeScreenTeacher.fxml").openStream());
+		  Scene scene = new Scene(root);   
+		  primaryStage.setScene(scene);  
+		  primaryStage.show();
+	    // do what you have to do
+	    //stage.close();
+	}
+	
+	protected ChatClient chat;
+	
+	String[] messageToServer = new String[3];
+	/* connections variables */
+	protected String ip ;// server ip
+	
+	final public static int DEFAULT_PORT = 5555;
   
-  / this method connected between client and server /
- public void connect(UserControl user){
-  try {
-   chat = new ChatClient(ip, DEFAULT_PORT, user);
-  } catch (IOException exception) {
-   System.out.println("Error: Can't setup connection!" + " Terminating client.");
-   System.exit(1);
-  }
- }
- 
+  /* this method connected between client and server */
+	public void connect(UserControl user){
+		try {
+			chat = new ChatClient(ip, DEFAULT_PORT, user);
+		} catch (IOException exception) {
+			System.out.println("Error: Can't setup connection!" + " Terminating client.");
+			System.exit(1);
+		}
+	}
+	
 
- public void initialize(URL arg0, ResourceBundle arg1) {
-  Scanner sc = new Scanner(System.in);
-  System.out.println("enter server ip");
-  this.ip = "77.138.70.98";
-  //this.ip = sc.nextLine();
-  sc.close();
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter server ip");
+		this.ip = "77.138.70.98";
+		//this.ip = sc.nextLine();
+		sc.close();
 
- }
- 
- 
- public void checkMessage(Object message) {
-  try {
-   chat.closeConnection();// close the connection
-   Object[] msg = (Object[]) message;
-   if(msg[1] == null)
-   {
-    System.out.println("no user");
-    return;
-   }
-   if (msg[0].toString().equals("checkUserDetails")) {
-    ArrayList<String> userDetails = (ArrayList<String>) msg[1];
-    if (userDetails == null) {
-     noUser.setDisable(false);
-     
-    } else {
-     switch (userDetails.get(2).toLowerCase()) {
-     case "teacher": {
-      //Platform.exit();
-     
-      System.out.println("teacher screen request");
-      Platform.runLater(new Runnable() {
+	}
+	
+	
+	public void checkMessage(Object message) {
+		try {
+			chat.closeConnection();// close the connection
+			Object[] msg = (Object[]) message;
+			if(msg[1] == null)
+			{
+				System.out.println("no user");
+				return;
+			}
+			if (msg[0].toString().equals("checkUserDetails")) {
+				ArrayList<String> userDetails = (ArrayList<String>) msg[1];
+				if (userDetails == null) {
+					noUser.setDisable(false);
+					
+				} else {
+					switch (userDetails.get(2).toLowerCase()) {
+					case "teacher": {
+						//Platform.exit();
+					
+						System.out.println("teacher screen request");
+						Platform.runLater(new Runnable() {
 
-       @Override
-       public void run() {
-        // TODO Auto-generated method stub
-         try {
-         home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenTeacher.fxml"));
-             home_page_scene = new Scene(home_page_parent);
-           Main.getStage().setScene(home_page_scene);
-        } catch (IOException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-        }
-       
-            
-       }
-       
-      });
-      
-      
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								 try {
+									home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenTeacher.fxml"));
+								     home_page_scene = new Scene(home_page_parent);
+									  Main.getStage().setScene(home_page_scene);
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							
+							     
+							}
+							
+						});
+						
+						
 
-         
+					    
 
-      break;
-     }
-     case "student": {
-      System.out.println("student screen request");
-      Platform.runLater(new Runnable() {
+						break;
+					}
+					case "student": {
+						System.out.println("student screen request");
+						Platform.runLater(new Runnable() {
 
-       @Override
-       public void run() {
-        // TODO Auto-generated method stub
-         try {
-         home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenStudent.fxml"));
-             home_page_scene = new Scene(home_page_parent);
-           Main.getStage().setScene(home_page_scene);
-        } catch (IOException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-        }
-       
-            
-       }
-       
-      });
-      
-      break;
-     }
-     case "director": {
-      System.out.println("director screen request");
-      Platform.runLater(new Runnable() {
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								 try {
+									home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenStudent.fxml"));
+								     home_page_scene = new Scene(home_page_parent);
+									  Main.getStage().setScene(home_page_scene);
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							
+							     
+							}
+							
+						});
+						
+						break;
+					}
+					case "director": {
+						System.out.println("director screen request");
+						Platform.runLater(new Runnable() {
 
-       @Override
-       public void run() {
-        // TODO Auto-generated method stub
-         try {
-         home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenDirector.fxml"));
-             home_page_scene = new Scene(home_page_parent);
-           Main.getStage().setScene(home_page_scene);
-        } catch (IOException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-        }
-       
-            
-       }
-       
-      });
-      break;
-     }
-     }
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								 try {
+									home_page_parent = FXMLLoader.load(getClass().getResource("/boundary/HomeScreenDirector.fxml"));
+								     home_page_scene = new Scene(home_page_parent);
+									  Main.getStage().setScene(home_page_scene);
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+							
+							     
+							}
+							
+						});
+						break;
+					}
+					}
 
-    }
+				}
 
-   }
-  } catch (Exception e) {
-   e.printStackTrace();
-  }
- }
-  public void loginPressed(ActionEvent e) throws IOException {
-    connect(this);
-    if (userName.getText() == null || password.getText() == null)
-     loginError.setDisable(false);
-    else {
-     messageToServer[0] = "checkUserDetails";
-     messageToServer[1] = userName.getText();
-     messageToServer[2] = password.getText();
-     chat.handleMessageFromClientUI(messageToServer);
-    }
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	 public void loginPressed(ActionEvent e) throws IOException {
+		  connect(this);
+		  if (userName.getText() == null || password.getText() == null)
+		   loginError.setDisable(false);
+		  else {
+		   messageToServer[0] = "checkUserDetails";
+		   messageToServer[1] = userName.getText();
+		   messageToServer[2] = password.getText();
+		   chat.handleMessageFromClientUI(messageToServer);
+		  }
 
-   }
+		 }
 
- }
+	}
+
+
