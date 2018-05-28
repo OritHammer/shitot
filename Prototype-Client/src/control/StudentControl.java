09f84524e8a8c041f619a5ada158c8f087294581
lior hammer <lior hammer@DESKTOP-812DTM7>
2018-05-28 12:14:58 +0300

    continue working on student window

--- Prototype-Client/src/control/StudentControl.java
+++ Prototype-Client/src/control/StudentControl.java
@@ -1,10 +1,15 @@
 package control;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -15,7 +20,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class StudentControl  extends UserControl implements Initializable{
-
+/********************* Variable declaration *************************/
 	@FXML
 	private Label userNameLabel ;
 	@FXML 
@@ -41,13 +46,13 @@ public class StudentControl  extends UserControl implements Initializable{
 	
 	@FXML
 	private TableView<String> examsTable;
-	
-	
-	
+	/************* Class Useful variables *************************/
+	String subjectChoosen ;
+	String[] messageToServer = new String[3];
 	
 	
 	/***************Class Methods*******************************/
-	public void  windowUp(String sName) {
+	@FXML public void  showDetails(String sName) {
 		userNameLabel.setText(sName); // setting student name
 		authorLabel.setText("Student"); // setting authorize
 		//Calendar cal = Calendar.getInstance();
@@ -55,10 +60,31 @@ public class StudentControl  extends UserControl implements Initializable{
 		Date todayDate = new Date();
 		dateLabel.setText(todayDate.toString());
 		//DateFormat dateForm = new SimpleDateFormat("DD/MM/YY");
-	//	String formatDateString = dateForm.format(todayDate);
-	
-		
+	//	String formatDateString = dateForm.format(todayDate);	
 	}
+	/******************************MyGradesWindow*********************************/
+	/**************************Action Listeners***********************/
+	public void showGradesOpened(ActionEvent e) throws IOException{
+		//ask for relevant subject from the server 
+		messageToServer[0] = "getSubjects";
+		messageToServer[1] = null;
+		messageToServer[2] = null;
+		chat.handleMessageFromClientUI(messageToServer);// send the message to server	
+	}
+	public void chooseSubjectPressed(ActionEvent e) throws IOException  {
+		choosingSubject.getSelectionModel().clearSelection();
+		//clearing previous results  
+		for(int i=0;i<examsTable.getItems().size();i++)
+			examsTable.getItems().clear();
+		//get the chosen subject by the user
+		subjectChoosen = choosingSubject.getValue();
+		//ask for executed exams for the chosen subject from the server
+		messageToServer[0] = "getExecutedExams";
+		messageToServer[1] = subjectChoosen;
+		messageToServer[2] = null;
+		chat.handleMessageFromClientUI(messageToServer);// send the message to server	
+	}
+/******************** LoadingDate methods to GUI ********************/
 	
 	
 	
