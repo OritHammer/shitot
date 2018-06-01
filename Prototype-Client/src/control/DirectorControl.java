e87072080969863bd30eb2316a1ea28ac269b7de
win10 <win10@DESKTOP-SDVAVFE>
2018-06-01 13:08:37 +0300

    for orit

--- Prototype-Client/src/control/DirectorControl.java
+++ Prototype-Client/src/control/DirectorControl.java
@@ -1,14 +1,44 @@
 package control;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class DirectorControl extends UserControl implements Initializable {
 
+	/// HOME TAB
+
+	
+	//FAML Adding Time Requests TAB
+	@FXML 
+	private ComboBox<String> cmbATRChooseExecutedExam;
+	@FXML 
+	private TextField txtFATRCourseName;
+	@FXML 
+	private TextField txtFATRTeachName;
+	@FXML 
+	private TextField txtFATRTimeAdded;
+	@FXML 
+	private TextField txtFATRreasonAddingTime;
+	@FXML
+	private Button btnATRApprove;
+	@FXML
+	private Button btnATRreject;
+	//FXML Statistic Reports
 	
 	
 	
+	//FXML System information
 	
-	
+	public void initialize(URL url, ResourceBundle rb) {
+		userText.setText(userNameFromDB);
+	}
 	
 	
 	
