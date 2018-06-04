608d37785c671ec57d5c6ff70182e25edc4fa33d
orit1 <orit1@DESKTOP-2969IQS>
2018-06-04 16:37:12 +0300

    working on director orit

--- Prototype-Client/src/control/DirectorControl.java
+++ Prototype-Client/src/control/DirectorControl.java
@@ -3,8 +3,12 @@ package control;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
+import entity.TeachingProfessionals;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,6 +31,15 @@ public class DirectorControl extends UserControl implements Initializable {
 	private Label userText1;
 	@FXML
 	private Label pageLabel;
+	@FXML
+	private Button AddingTime;
+	@FXML
+	private Button StatisticReport;
+	@FXML
+	private Button SystemInformation;
+	@FXML
+	private Button backButton;
+	
 	//FAML Adding Time Requests TAB
 	@FXML 
 	private ComboBox<String> cmbATRChooseExecutedExam;
@@ -43,23 +56,13 @@ public class DirectorControl extends UserControl implements Initializable {
 	@FXML
 	private Button btnATRreject;
 	
-	@FXML
-	private Button AddingTime;
-	@FXML
-	private Button StatisticReport;
-	@FXML
-	private Button SystemInformation;
-	@FXML
-	private Button backButton;
-	
-	
-	
+
 	//FXML Statistic Reports
 	
 	
 	
 	//FXML System information
-	
+/*********************************************************************homePageButtons**********************************************************/
 	public void initialize(URL url, ResourceBundle rb) {
 		 if(pageLabel.getText().equals("Home screen"))
 			  userText1.setText(Globals.userName);
@@ -67,6 +70,10 @@ public class DirectorControl extends UserControl implements Initializable {
 	public void openAddingTimeRequest(ActionEvent e) {
 		((Node)e.getSource()).getScene().getWindow().hide(); //hiding homePage window
 		openScreen("addingTimeRequestDirector");
+		messageToServer[0] = "getExecutedExamCodeList";
+		messageToServer[1] = null;
+		messageToServer[2] = null;
+		chat.handleMessageFromClientUI(messageToServer);// send the message to server
 	}
 	public void openStatisticReport(ActionEvent e) {
 		((Node)e.getSource()).getScene().getWindow().hide(); //hiding homePage window
@@ -76,13 +83,11 @@ public class DirectorControl extends UserControl implements Initializable {
 		((Node)e.getSource()).getScene().getWindow().hide(); //hiding homePage window
 		openScreen("systemInformationDirector");
 	}
-	private void openScreen(String screen) {
+	private void openScreen(String screen) {//open the windows after login
 		try{
 			Stage primaryStage = new Stage();
 			FXMLLoader loader = new FXMLLoader();
-			Pane root = loader.load(getClass().getResource("/directorBoundary/"+screen+".fxml").openStream());
-			
-
+			Pane root = loader.load(getClass().getResource("/directorBoundary/"+screen+".fxml").openStream());///////////*נצטרך לשנות לבונדרי רגיל או להתאים לארור מסג'*/////////////
 			
 			Scene scene = new Scene(root);			
 			 if(screen.equals("ErrorMessage")) {
@@ -91,7 +96,7 @@ public class DirectorControl extends UserControl implements Initializable {
 	    			tController.setErrorMessage("ERROR"); //send a the error to the alert we made
 	            }
 	        primaryStage.setTitle(screen);
-	    //   primaryStage.getIcons().add(new Image("/Prototype-Client/Copywriting-Master-Class-Owl-200x175.png"));
+	   //     primaryStage.getIcons().add(new Image("/Prototype-Client/Copywriting-Master-Class-Owl-200x175.png"));
 			primaryStage.setScene(scene);		
 			primaryStage.show();
 			
@@ -101,7 +106,7 @@ public class DirectorControl extends UserControl implements Initializable {
 	        	  System.out.println("Error in opening the page");
 	          }		
 	}
-	private void openScreen(String screen,String message) {
+	private void openScreen(String screen,String message) {// for error message
 		try{
 				FXMLLoader loader=new FXMLLoader();
 				loader.setLocation(getClass().getResource("/directorBoundary/"+screen+".fxml"));
@@ -117,11 +122,45 @@ public class DirectorControl extends UserControl implements Initializable {
 	        	  System.out.println("Error in opening the page");
 	          }		
 	}
-	/*back button was pressed*/
+	/*cancel button was pressed*/
 	public void backButtonPressed(ActionEvent e) throws IOException, SQLException {
 	    final Node source = (Node) e.getSource();
 	    Stage stage = (Stage) source.getScene().getWindow();
 	    stage.close();	
 	    openScreen("HomeScreenDirector");
+	}
+///////********************************************check the message that arrived from server***********************************************************/
+	public void checkMessage(Object message) {
+		try {
+			chat.closeConnection();// close the connection
+
+			Object[] msg = (Object[]) message;
+			/*	msg[1]=new ArrayList<String>();
+				((ArrayList<String>)msg[1]).add("afdvdfv");
+				((ArrayList<String>)msg[1]).add("bhhgk");            */
+			switch (msg[0].toString()) {
+			case ("getTimeRequestCodeList"): { /* get the subjects list from server */
+				initExecutedExamList((ArrayList<String>) msg[1]);
+				break;
+			}
+			case ("bkjb"):{
+				
+				break;
+			}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IndexOutOfBoundsException e) {
+			e.printStackTrace();
+		}
+	}
+	/*******************************************************listeners on addingTimeRequestDirector***********************************************************/
+	/**/
+	public void initExecutedExamList(ArrayList<String> ExecutedExamList){
+		ObservableList<String> observableList = FXCollections.observableArrayList(ExecutedExamList);
+		cmbATRChooseExecutedExam.setItems(observableList);
+		
+		//questionsComboBox.getItems().remove(questionsComboBox.getValue());//removing from combobox
 	}
 }
