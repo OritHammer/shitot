e9fd7d2c20371dae380329682dedc2385806b0c1
lior hammer <lior hammer@DESKTOP-812DTM7>
2018-06-04 19:21:49 +0300

    continue working Student

---
+++ Prototype-Client/src/entity/StudentPerformExam.java
@@ -0,0 +1,78 @@
+package entity;
+
+import java.io.Serializable;
+
+public class StudentPerformExam implements Serializable{
+	private String date = " ";
+	private String time = " ";
+	private String finished = "0" ;
+	private String excecutedExamID = " ";
+	private String userName = " ";
+	private String isApproved = "0" ;
+	private String resonForChangeGrade = " "; 
+	
+	public StudentPerformExam(String dateDB,String timeDB, String finishedDB,String excecutedExamIDDB,String userNameDB,String isApprovedDB,String resonForChangeGradeDB) {
+		super();
+		date = dateDB;
+		time = timeDB;
+		finished = finishedDB;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public String getFinished() {
+		return finished;
+	}
+
+	public void setFinished(String finished) {
+		this.finished = finished;
+	}
+
+	public String getExcecutedExamID() {
+		return excecutedExamID;
+	}
+
+	public void setExcecutedExamID(String excecutedExamID) {
+		this.excecutedExamID = excecutedExamID;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getIsApproved() {
+		return isApproved;
+	}
+
+	public void setIsApproved(String isApproved) {
+		this.isApproved = isApproved;
+	}
+
+	public String getResonForChangeGrade() {
+		return resonForChangeGrade;
+	}
+
+	public void setResonForChangeGrade(String resonForChangeGrade) {
+		this.resonForChangeGrade = resonForChangeGrade;
+	}
+	
+	
+}
